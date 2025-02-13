b07ce52954f06abe1955dce8e3a5565739870602
xSergioz <1698224@alu.murciaeduca.es>
2025-02-13 13:30:17 +0100

    Commit de seguridad
    Se ha petado el ordenador, por si acaso, hago un commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes} from 'react-router-dom';
+import Alumno from './paginas/Alumno';
+import CentroEducativo from './paginas/CentroEducativo';
+import Empresa from './paginas/Empresa';
+import Inicio from './paginas/Inicio';
+import Cabecera from './componentes/Cabecera';
+import Pie from './componentes/Pie';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <>
+  <Cabecera />
+  </>
+  
 }
 
 export default App
